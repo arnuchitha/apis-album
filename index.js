b618ee00b305129b95219622e2f76e32a9b007b2
arnuchitha <ha.shownoob@gmail.com>
2024-02-14 16:10:15 +0700

    update

--- index.js
+++ index.js
@@ -18,20 +18,17 @@ global.PATHIMG = __dirname+'/public/images';
 
 
 app.use(express.json());
+
 const pathname = process.env.VERSION;
 app.get(pathname+'/', (req, res) => {
-    console.log("in")
     res.send('Welcome to zegotravel apis album');
 });
 
 const albumRoute = require('./routes/alubumRoute');
 
 app.use(`${pathname}/albumview`,verify,albumRoute);
-console.log(`${pathname}/albumview`)
-// app.listen(port, () => {
-//     console.log('Start server at port 3000.')
-// })
 
 app.listen(process.env.PORT);
+
 
 module.exports = app;
