943713da5c69b40cea10f0bf974624c6e10fe1bf
arnuchitha <ha.shownoob@gmail.com>
2024-02-20 11:01:17 +0700

    update

--- models/album.js
+++ models/album.js
@@ -2,60 +2,50 @@ var moment = require("moment");
 var _ = require("lodash");
 const glob = require('glob');
 const _fs = require('fs').promises;
-// const jsfiles = glob.sync('**/*.js', { ignore: 'node_modules/**' })
-// // const _db = require("../core/crudMysql");
-
-// const stopAfter100ms = glob('**/*.css', {
-//     signal: AbortSignal.timeout(100),
-// })
-
-// const images = glob.sync(['css/*.{png,jpeg}', 'public/*.{png,jpeg}'])
-
-
-// ใช้ glob() เพื่อค้นหาไฟล์ที่ตรงกับรูปแบบที่กำหนด
-// glob('*.js', function (err, files) {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log(files);
-//   }
-// });
-
-// หรือใช้ glob.sync() เพื่อค้นหาไฟล์ที่ตรงกับรูปแบบที่กำหนด โดยไม่ใช้ callback
-const files = glob.sync('*.js');
-console.log(files);
+var path = require("path");
+const multer = require('multer');
 
 class Album {
     constructor() {}
-    // async checkFolderMantenance(){
-    //     let isProblemFileSrcIn = isProblem.filter(o=>o==false).length==0; 
-    //     //check warnning path Maintenance
-    //     let localPath  =`${process.cwd()}`;
-    //     let isNotProblemWorkDone = await validPath(path.join(localPath,'\\workdone'));
-    //     isProblemAll.push(isNotProblemWorkDone);
-    
-    //     //มี Folder  workdone ให้ทำงานเสมอ
-    //     // เช็คที่อยู่ Workdone มีมั้ย เช็คที่อยู่แฟ้ม Maintenance ที่จะเก็บมีมั้ย เช็คชื่อไฟล์ที่เก็บถูกต้องมั้ย
-    //     if (isNotProblemWorkDone && isProblemSrcIn && isProblemFileSrcIn) {
-    //         let isProblemDesIn = await validPath(`${localPath}${cnf.desIn}`);
-    //         if (!isProblemDesIn) {
-    //             await _fs.mkdir(`${localPath}${cnf.desIn}`).catch((error)=>{
-    //                 isProblemAll.push(false);
-    //                 _elog.winston().warn({event:"DIRECTORY",process:"MN-APMAS",message:`Found Problem Code: ${error.code} ${localPath}${cnf.desIn}`,data:{}});  
-    //                 return false              
-    //             });
-    
-    //         }
-    //     }
-    //     isProblemAll.push(isProblemSrcIn,isProblemFileSrcIn);
-    //     await timeout(500);
-    //     return isProblemAll.filter((o)=>{ return o==false}).length==0
-    // }
 
-    async getTest() {
-        const jsfiles = glob.sync('**/*.js', { ignore: 'node_modules/**' })
-        console.log("TEST", jsfiles);
-    }
+    async uploadfile () {
+        const storage = multer.diskStorage({
+            destination: (req, file, cb) => {
+              cb(null, 'japan/'); // กำหนดให้เก็บไฟล์ในโฟลเดอร์ uploads
+            },
+            filename: (req, file, cb) => {
+              cb(null, `${Date.now()}${path.extname(file.originalname)}`); // กำหนดชื่อไฟล์ให้ไม่ซ้ำกัน
+            }
+        });
+    
+        const upload = multer({ storage });
+    };
+
+    async createFolder(albumNameValue, countryNameValue, cityNameValue) {
+        console.log("IN CREATE");
+        console.log(albumNameValue, countryNameValue, cityNameValue);
+
+        try {
+            const folderPath = glob.sync(`all-album/${countryNameValue}/`);
+            console.log(folderPath);
+
+            await _fs.mkdir(`${folderPath[0]}${albumNameValue}`); // สร้างแฟ้มในโฟลเดอร์ปัจจุบัน
+            // res.status(201).send(`Directory '${albumNameValue}' created successfully`);
+
+            const folderName = glob.sync(`all-album/${countryNameValue}/*`);
+            // folderName.forEach(file => {
+            //     console.log(file);
+            // });
+
+            return true;
+            
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Error creating directory');
+        }
+
+    };
+    
   }
   
   module.exports = Album;
--- routes/alubumRoute.js
+++ routes/alubumRoute.js
@@ -1,12 +1,45 @@
 var express = require('express');
 var router = express.Router();
 const album = require('../models/album');
+const path = require('path');
+const { fstat } = require('fs');
+const _fs = require('fs').promises;
 
 router.get('/', async function (req, res) {
     let ab = new album();
     let result = await ab.getTest();
     res.status(200).send(result);
 });
+// router.get('/dataAlbum', async function (req, res) {
+//     let ab = new album();
+//     let result = await ab.getTest();
+//     res.status(200).sendFile(path.join(__dirname, 'index.html'));
+// });
+// router.post('/uploadAlbum', upload.single('image'), (req, res) =>{
+//     let ab = new album();
+//     let result = await ab.uploadfile();
+//     res.status(200).send(result);
+// });
+router.post('/createFolder', async function (req, res) {
 
+    let data = req.body;
+    let albumNameValue = data.albumName;
+    let countryNameValue = data.countryName;
+    let cityNameValue = data.cityName;
+    
+    let ab = new album();
+    let result = await ab.createFolder(albumNameValue, countryNameValue, cityNameValue);
+    console.log("1212",result)
+    // const directoryName = data.directoryName; // รับชื่อแฟ้มจาก request body
+    // console.log(directoryName);
+    // try {
+    //     await _fs.mkdir(path.join(__dirname, directoryName)); // สร้างแฟ้มในโฟลเดอร์ปัจจุบัน
+    //     res.status(201).send(`Directory '${directoryName}' created successfully`);
+    // } catch (err) {
+    //     console.error(err);
+    //     res.status(500).send('Error creating directory');
+    // }
 
+    res.status(200).send(true);
+});
 module.exports = router;
